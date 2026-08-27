4625f71653df6171e826f59167495e6ee94f3143
Aksh Khurana <150786734+iakshkhurana@users.noreply.github.com>
2026-08-27 18:40:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,32 +1,71 @@
+// class Solution {
+//   public:
+//     vector<int> topoSort(int V, vector<vector<int>>& edges) {
+//         // step 1 is to compute adjacency list
+//         vector<vector<int>> adj(V);
+//         for(auto& e: edges){
+//             adj[e[0]].push_back(e[1]);
+//         }
+//         // step 2 is to compute the indegree
+//         vector<int> indegree(V,0);
+//         for(int i=0;i<V;i++){
+//             for(auto it:adj[i]){
+//                 indegree[it]++;
+//             }
+//         }
+//         // step 3 is to do the main thing jisme 0 indegree wala kaam krna h
+//         queue<int>q;
+//         for(int i=0;i<V;i++){
+//             if(indegree[i]==0)  q.push(i);
+//         }
+//         // step 4 is to do bfs
+//         vector<int>topo;
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
+//             topo.push_back(node);
+//             for(auto it: adj[node]){
+//                 if(--indegree[it]==0)   q.push(it);
+//             }
+//         }
+//         return topo;
+//     }
+// };
+
 class Solution {
+  private:
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, stack<int>& st) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it, adj, vis, st);
+            }
+        }
+        st.push(node);   // saare neighbours ho gaye, ab push
+    }
+
   public:
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        // step 1 is to compute adjacency list
+        // 1. adjacency list (directed: u -> v)
         vector<vector<int>> adj(V);
-        for(auto& e: edges){
+        for (auto& e : edges) {
             adj[e[0]].push_back(e[1]);
         }
-        // step 2 is to compute the indegree
-        vector<int> indegree(V,0);
-        for(int i=0;i<V;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
+
+        // 2. DFS from every unvisited node
+        vector<int> vis(V, 0);
+        stack<int> st;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, adj, vis, st);
             }
         }
-        // step 3 is to do the main thing jisme 0 indegree wala kaam krna h
-        queue<int>q;
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0)  q.push(i);
-        }
-        // step 4 is to do bfs
-        vector<int>topo;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it: adj[node]){
-                if(--indegree[it]==0)   q.push(it);
-            }
+
+        // 3. stack se nikaalo
+        vector<int> topo;
+        while (!st.empty()) {
+            topo.push_back(st.top());
+            st.pop();
         }
         return topo;
     }
